2dae5f9e0d84ae008799445e58266ef2734737c8
SilentDD4 <dzurecd26@hsnet.ahsd.org>
2024-05-28 13:45:58 -0400

    Add files via upload

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Movement controller;
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 14f;
+    private bool isRight = true;
+    public bool jump = true;
 
-    public float runSpeed = 8f;
-
-    float horizontalMove = 0f;
-    float jump = 0;
-
-    void Update ()
-    {
-        horizontalMove = Input.getAxisRaw("Horizontal") * runSpeed;
-
-        if(Input.getButtonDown("Jump")){
-            jump++;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+//    updates once per frame to allow the character to remain horizontal
+    void Update(){
+        horizontal = Input.GetAxisRaw("Horizontal");
+//Jump detection
+        if(Input.GetButtonDown("Jump")){
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            jump = false;
         }
+
+         Flip(); 
     }
     void FixedUpdate () {
 //To move the character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, (jump == 0));
-        jump--;
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y); 
+    }
+
+    void onTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Ground")){
+            jump = true; 
+        }
+    }
+    private void Flip(){
+        if(isRight && horizontal < 0f || !isRight && horizontal > 0f){
+            isRight = !isRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
